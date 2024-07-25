e0c3268d3a90101bb1a9287b2ffcfc5ae17d18cc
Manarth Patel <manarthpatel237@gmail.com>
2024-07-24 23:23:30 -0400

    75% work done

--- smart-attend-backend/app/crud.py
+++ smart-attend-backend/app/crud.py
@@ -7,21 +7,21 @@ scheduled_classes_collection = get_scheduled_classes_collection()
 
 def create_student(student: Student):
     student.student_id = generate_student_id()
-    student_collection.insert_one(student.dict())
+    student_collection.insert_one(student.model_dump())
 
 def get_students():
     return list(student_collection.find({}, {'_id': 0}))
 
 def create_attendance_record(record: AttendanceRecord):
     attendance_collection = get_attendance_collection(record.course)
-    attendance_collection.insert_one(record.dict())
+    attendance_collection.insert_one(record.model_dump())
 
 def get_attendance_records(course):
     attendance_collection = get_attendance_collection(course)
     return list(attendance_collection.find({}, {'_id': 0}))
 
 def create_scheduled_class(scheduled_class: ScheduledClass):
-    scheduled_classes_collection.insert_one(scheduled_class.dict())
+    scheduled_classes_collection.insert_one(scheduled_class.model_dump())
 
 def get_scheduled_classes():
     return list(scheduled_classes_collection.find({}, {'_id': 0}))
--- smart-attend-backend/app/database.py
+++ smart-attend-backend/app/database.py
@@ -4,10 +4,17 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client['attendance']
 
 def get_student_collection():
+    if 'students' not in db.list_collection_names():
+        db.create_collection('students')
     return db['students']
 
 def get_attendance_collection(course):
-    return db[f'attendance_{course}']
+    collection_name = f'attendance_{course}'
+    if collection_name not in db.list_collection_names():
+        db.create_collection(collection_name)
+    return db[collection_name]
 
 def get_scheduled_classes_collection():
+    if 'scheduled_classes' not in db.list_collection_names():
+        db.create_collection('scheduled_classes')
     return db['scheduled_classes']
